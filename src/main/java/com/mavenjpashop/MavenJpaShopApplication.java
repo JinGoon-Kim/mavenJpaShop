bd7842161453eff41ba1abd9c56b36d0f1f99c3a
JinGoon-Kim <raneata@gmail.com>
2022-01-16 20:07:57 +0900

    update pom.xml

---
+++ src/main/java/com/mavenjpashop/MavenJpaShopApplication.java
@@ -0,0 +1,13 @@
+package com.mavenjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenJpaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mavenjpashop/MavenJpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavenjpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJpaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
