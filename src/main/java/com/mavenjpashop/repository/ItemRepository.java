3837543a0241261071a36d69e7654e966bb6889e
JinGoon-Kim <raneata@gmail.com>
2022-01-17 22:02:14 +0900

    added test

--- src/main/java/com/mavenjpashop/repository/ItemRepository.java
+++ src/main/java/com/mavenjpashop/repository/ItemRepository.java
@@ -3,6 +3,11 @@ package com.mavenjpashop.repository;
 import com.mavenjpashop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    
+
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String ItemDetail);
+    List<Item> findByPriceLessThan(Integer price);
 }
