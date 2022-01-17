6bb16639e9cb4dd921236fb7de4a4e6cb573e5eb
JinGoon-Kim <raneata@gmail.com>
2022-01-17 22:15:32 +0900

    added Query and NaiveQuery with Test

--- src/main/java/com/mavenjpashop/repository/ItemRepository.java
+++ src/main/java/com/mavenjpashop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.mavenjpashop.repository;
 
 import com.mavenjpashop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,11 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNm(String itemNm);
     List<Item> findByItemNmOrItemDetail(String itemNm, String ItemDetail);
     List<Item> findByPriceLessThan(Integer price);
+
+    @Query("SELECT i FROM Item i WHERE i.itemDetail LIKE %:itemDetail% ORDER BY i.price DESC")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value = "SELECT * FROM item i WHERE i.item_detail LIKE " +
+            "%:itemDetail% ORDER BY i.price DESC", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 }
