9be16179bb7084375b4a964f173333b1fdb72f7f
JinGoon-Kim <raneata@gmail.com>
2022-02-03 21:22:01 +0900

    create entity Order and OrderItem

--- src/main/java/com/mavenjpashop/entity/Order.java
+++ src/main/java/com/mavenjpashop/entity/Order.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -25,7 +27,12 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;    // 주문상태
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     private LocalDateTime regTime;
 
     private LocalDateTime updateTime;
+
+
 }
