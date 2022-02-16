b829ab458b716ddaa74a9d524071018235c1df64
JinGoon-Kim <raneata@gmail.com>
2022-02-16 21:47:01 +0900

    added item image upload

---
+++ src/main/java/com/mavenjpashop/repository/ItemImgRepository.java
@@ -0,0 +1,8 @@
+package com.mavenjpashop.repository;
+
+import com.mavenjpashop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
